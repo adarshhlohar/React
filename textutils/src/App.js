e5428a5dfbab2e2d270d72a9cb124698a407d331
Adarsh <adarshhlohar@gmail.com>
2023-08-19 22:16:41 +0530

    Adding todays file 19-08-23

---
+++ textutils/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './component/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" about="About Text"/>
+      {/* <Navbar/> */}
+      </>
+  );
+}
+
+export default App;
